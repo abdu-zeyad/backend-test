e100d125925210fd23234b1e11653729a2e7aa75
abdu-zeyad <abd.d@live.com>
2021-07-06 11:37:53 +0300

    adding alldata

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT;
 
+// routs
+app.get("/all", getAllDataHandler);
+
+//
+//handlers
+//
+function getAllDataHandler(req, res) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`;
+  axios.get(url).then((results) => {
+    res.send(results.data.drinks);
+  });
+}
+
+//
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
