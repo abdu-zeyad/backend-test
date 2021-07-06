d68a4b81a9cd7be2800de7ee363b9ca9b1d2649c
abdu-zeyad <abd.d@live.com>
2021-07-06 12:26:08 +0300

    bult the server

--- server.js
+++ server.js
@@ -4,11 +4,25 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/drinkdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const drinksSchema = new mongoose.Schema({
+  strDrink: String,
+  strDrinkThumb: String,
+});
+
+const Drinks = mongoose.model("drinks", drinksSchema);
 
 // routs
 app.get("/all", getAllDataHandler);
-
+app.post("/addtofav", addtofavhandler);
+app.get("/getfavdata", addfavorite);
 //
 //handlers
 //
@@ -19,6 +33,20 @@ function getAllDataHandler(req, res) {
   });
 }
 
+function addtofavhandler(req, res) {
+  const { strDrink, strDrinkThumb } = req.body;
+  console.log(strDrink);
+  const newdrink = new Drinks({
+    strDrink: strDrink,
+    strDrinkThumb: strDrinkThumb,
+  });
+  newdrink.save();
+}
+function addfavorite(req, res) {
+  Drinks.find({}, (err, data) => {
+    res.send(data);
+  });
+}
 //
 app.get("/", function (req, res) {
   res.send("Hello World");
